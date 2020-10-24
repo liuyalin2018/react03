6e550bba57966ffa892eb1160dc7f6617f68a0dc
liuyalin2018 <2083805059@qq.com>
2020-10-24 09:37:28 +0800

    taobao

---
+++ build/precache-manifest.6144618296863e06433581c0c4ff6bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4e27e29dd537fdcb8da8e374253050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec11f988507789931d02",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6b5f2ac6a6e65b285672",
+    "url": "/static/css/main.218b9c41.chunk.css"
+  },
+  {
+    "revision": "ec11f988507789931d02",
+    "url": "/static/js/2.141121ec.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.141121ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5f2ac6a6e65b285672",
+    "url": "/static/js/main.e8873e56.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
